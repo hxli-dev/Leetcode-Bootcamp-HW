ccddc7f701e1b91e673446bb542717c780a3c5c8
Hongxin Li <82593585+hxli-dev@users.noreply.github.com>
2025-11-04 11:41:52 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        ans = [0] * n
+        stack = []   
+
+        for i, t in enumerate(temperatures):
+
+            while stack and t > temperatures[stack[-1]]:
+                j = stack.pop()
+                ans[j] = i - j
+            stack.append(i)
+
+        return ans
